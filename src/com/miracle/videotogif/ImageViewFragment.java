7bc87234d63a5cb93a76d169360e82c57ddf9bf0
Chengwen <chengwen.sui@gmail.com>
2014-08-15 00:48:33 -0400

    update gif show in browser

--- src/com/miracle/videotogif/ImageViewFragment.java
+++ src/com/miracle/videotogif/ImageViewFragment.java
@@ -1,8 +1,13 @@
 package com.miracle.videotogif;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import org.ffmpeg.android.Clip;
 
@@ -13,10 +18,12 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -35,6 +42,52 @@ public class ImageViewFragment extends Fragment {
 
 		parentView = inflater.inflate(R.layout.imageview, container, false);
 
+		if (MenuActivity.imageURL != null) {
+		byte[] imageRaw = null;
+		  try {
+		     //URL url = new URL(MenuActivity.imageURL);
+		     //HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+		     //InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+		     
+		     File file = new File(MenuActivity.imageURL);
+		     InputStream in = null;
+		     in = new BufferedInputStream(new FileInputStream(file));
+		     
+		     ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+		     int c;
+		     while ((c = in.read()) != -1) {
+		         out.write(c);
+		     }
+		     out.flush();
+
+		     imageRaw = out.toByteArray();
+
+		     in.close();
+		     out.close();
+		  } catch (IOException e) {
+		     // TODO Auto-generated catch block
+		     e.printStackTrace();
+		  }
+
+		  String image64 = Base64.encodeToString(imageRaw, Base64.DEFAULT);
+
+		  String urlStr   = "http://example.com/my.gif";
+		  String mimeType = "text/html";
+		  String encoding = null;
+		  String pageData = "<img src=\"data:image/gif;base64," + image64 + "\" />";
+
+		  WebView wv;
+		  wv =  (WebView) parentView
+					.findViewById(R.id.webImageView1);
+		  wv.loadDataWithBaseURL(urlStr, pageData, mimeType, encoding, urlStr);
+		  
+		//WebView webImageView1 = (WebView) parentView
+		//		.findViewById(R.id.webImageView1);
+		//webImageView1.loadData("<html><img src='file:/"+MenuActivity.imageURL+"'/></html>",  "text/html", "UTF-8");
+	}
+		/*
 		gifimage = (ImageView) parentView
 				.findViewById(R.id.gifimage);
 		if (MenuActivity.imageURL != null) {
@@ -48,7 +101,7 @@ public class ImageViewFragment extends Fragment {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}
+		}*/
 
 		cancelbtn = (Button) parentView.findViewById(R.id.cancelbtn);
 
@@ -119,7 +172,7 @@ public class ImageViewFragment extends Fragment {
 					public void onClick(View view) {
 
 						Intent shareIntent = new Intent(Intent.ACTION_SEND);
-						shareIntent.setType("image/*");
+						shareIntent.setType("image/gif");
 						Uri uri = Uri.fromFile(new File(
 								MenuActivity.imageURL));
 						shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
